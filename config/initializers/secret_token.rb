4e1e966ff749f822bf59faccb2cecde9c119cf05
lala <whatwasthatonethen@gmail.com>
2014-04-04 05:05:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ridiculous::Application.config.secret_key_base = '6ca543ca0b7d12cc8e6cf7798965afba6962d79600b338e8391a1145574074ed1da5b2ff306c315432939fec65886e5316287afc15cdb14e5445006a52d861a5'
