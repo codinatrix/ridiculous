1c5427d1c903246e5392735127bcc55c0981856b
lala <whatwasthatonethen@gmail.com>
2014-04-04 09:38:00 +0200

    Added validations to supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,7 @@
 class Supplier < ActiveRecord::Base
+  
+  validates :name, :address, :latitude, :longitude, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: {maximum: 200}
+  validates :address, length: {maximum: 500}
 end
