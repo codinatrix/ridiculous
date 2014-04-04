e637dc1ec48a9f44cbeaa87ee154955f83a3e62e
lala <whatwasthatonethen@gmail.com>
2014-04-04 08:56:44 +0200

    Markers for each supplier are now shown on map

--- app/assets/javascripts/ridiculous.js
+++ app/assets/javascripts/ridiculous.js
@@ -3,10 +3,9 @@ var RIDICULOUSHAT = function() {
 	var mapClass = function() {
 		
 		var self = this;
-		var centerLoc;
+		var centerLoc, infoWindow, map;
 		
 		this.makeMap = function() {
-			centerLoc = new google.maps.LatLng(56.87963, 14.80671);
 			var mapOptions = {
 	          center: centerLoc,
 	          zoom: 15,
@@ -14,12 +13,41 @@ var RIDICULOUSHAT = function() {
 			  streetViewControl:false,
 			  overviewMapControl:false,
 	        };
-			var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+			map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		}
+		
+		this.createMarker = function(latlng, name, address) {
+			var html = "<b>" + name + "</b> <br/>" + address;
+		  	var marker = new google.maps.Marker({
+				map: map,
+				position: latlng
+		  	});
+		  	
+		  	google.maps.event.addListener(marker, 'click', function() {
+				infoWindow.setContent(html);
+				infoWindow.open(map, marker);
+		  	});
+		}
+		
+		this.setMarkers = function() {
+			var markers = document.getElementsByTagName("marker");
+			for (i = 0; i < markers.length; i++) {
+				var name = markers[i].getAttribute("name");
+				var address = markers[i].getAttribute("address");
+				var latlng = new google.maps.LatLng(
+	              parseFloat(markers[i].getAttribute("lat")),
+	              parseFloat(markers[i].getAttribute("lng")));
+				self.createMarker(latlng, name, address);
+			}
 		}
 		
 		this.init = function() {
-			
+			centerLoc = new google.maps.LatLng(56.87963, 14.80671);
+			infoWindow = new google.maps.InfoWindow({
+			    pixelOffset: new google.maps.Size(-9, 0)
+		    });
 			self.makeMap();
+			self.setMarkers();
 		}		
 	}// End of mapClass
 	
