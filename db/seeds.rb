3f11169d58cbfbb0fc8cfcba25eba9a362c0f831
lala <whatwasthatonethen@gmail.com>
2014-04-04 07:45:55 +0200

    Added seed data for suppliers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#encoding: utf-8
+#   Supplier.create(name: '', address: '', latitude: '', longitude: '')
+
+Supplier.create(name: 'Moe\'s Milk', address: 'Sandgärdsgatan 6 352 30 Växjö', latitude: '56.87778', longitude: '14.80793')
+Supplier.create(name: 'Cake Sweet Cake Bakery', address: 'Linnégatan 352 33 Växjö ', latitude: '56.88018', longitude: '14.81183')
+Supplier.create(name: 'Grumpy Ted\'s Red Meat', address: 'Östregårdsgatan 2 352 34 Växjö', latitude: '56.87821', longitude: '14.81303')
+
+
