13a55a3ad02ba135f12d8b5e62c9e99179cc564c
lala <whatwasthatonethen@gmail.com>
2014-04-06 08:52:50 +0200

    Added validations to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,10 @@ class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :suppliers, through: :taggings
   
+  # Fails silently. Reasoning: The only failed input is a duplicate or blank.
+  # The user doen't need to be told to delete duplicate or blank from input.
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  
   default_scope { order('name') }
 end
