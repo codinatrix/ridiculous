d25295adc875989d5ec840c8b93cd5ba79bbb584
lala <whatwasthatonethen@gmail.com>
2014-04-06 18:12:22 +0200

    Fields now editable by clicking icon

--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,2 +1,10 @@
 module SuppliersHelper
+  def edit_icon id, field
+    image_tag("/img/icons/edit.png", 
+                    :alt => "edit",
+                    :class => "edit_icon",
+                    :height => "12px",
+                    :width => "22px",
+                    :id => field + id.to_s )
+  end
 end
