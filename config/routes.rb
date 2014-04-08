33e4f96cab844d724b4c86eb5a5fa6a93ba42b9f
lala <whatwasthatonethen@gmail.com>
2014-04-08 14:21:06 +0200

    Fixed bug - logout didn't work w/ middle click
    Workaround for a devise bug. If user clicked on logout with the middle
    mouse button, it would try to fetch the logout view. This fix makes it
    redirect straight to root.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Ridiculous::Application.routes.draw do
   root to: 'suppliers#index'
   devise_for :users
   resources :suppliers
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
 end
