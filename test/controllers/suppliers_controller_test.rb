d1e796452722e9c0dba79db7f483c647ff90858e
lala <whatwasthatonethen@gmail.com>
2014-04-08 13:59:37 +0200

    Configured tests for devise

--- test/controllers/suppliers_controller_test.rb
+++ test/controllers/suppliers_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class SuppliersControllerTest < ActionController::TestCase
+  include Devise::TestHelpers                          
+  include Warden::Test::Helpers                        
+  Warden.test_mode! 
+  
+  def setup
+    @user = User.create(:email => 'steve@diamonds.com', :password => 'goldenapple')
+    sign_in @user
+  end
+  
+  
   test "should get index" do
     get :index
     assert_response :success
